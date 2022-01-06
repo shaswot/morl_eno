385b74c8aa4793773a3973e396558b94eb6599b2
shaswot <pratipada@silverstone>
2022-01-06 17:34:49 +0000

    updated docker starter script

--- start_docker_container.sh
+++ start_docker_container.sh
@@ -2,8 +2,8 @@
 docker run --gpus all \
         -dit \
         -v ~/stash:/stash \
-        --name pratipada\
-        -p 3310:8888 \
-        -p 3311:6006 \
-	bhootmali/morl_eno:pratipada \
+        --name <container_name>\
+        -p XXXX:8888 \
+        -p XXXX:6006 \
+	<image_name> \
 	screen -S jlab jupyter lab --no-browser --ip=0.0.0.0 --port 8888 --allow-root --LabApp.token=''
